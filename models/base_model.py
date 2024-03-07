0c097cd58a9b9618eb220b309f4cec06fee24356
mmanyuat <mmanyuat201@gmail.com>
2024-03-07 03:15:25 -0800

    task3

---
+++ models/base_model.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+""" Importing uuid_module to access the uuid4 function"""
+import uuid
+""" Importing datetime module specially datetime class"""
+
+
+class BaseModel:
+    """
+    A class that defines all common attributes for other classes
+    """
+
+    def __init__(self):
+        """ the class constructor """
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
+
+    def __str__(self):
+        """this method returns string representation of instance object"""
+        return ("[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__))
+
+    def save(self):
+        """Method that updates time to the current datetime"""
+
+        self.updated_at = datetime.now()
+
+    def to_dict(self):
+        """ Method that converts instance attributes into a dictionary"""
+
+        obj_dict = self.dict__.copy()
+        obj_dict = ['__class__'] = self.__class__.name__
+        obj_dict = ['created_at'] = self.created.isoformat()
+        obj_dict = ['updated_at'] = self.updated.isoformat()
+        return obj_dict
