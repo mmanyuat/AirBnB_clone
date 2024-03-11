e0a3f5c5ce0e7613e4768dcdeb6b040ffef72ae0
mmanyuat <mmanyuat201@gmail.com>
2024-03-11 00:29:42 -0700

    debug

--- models/base_model.py
+++ models/base_model.py
@@ -1,15 +1,15 @@
 #!/usr/bin/python3
 """ Importing uuid_module to access the uuid4 function & importing datetime"""
 import uuid
-from datetime import datetime
+import datetime
 
 
 def set_attribute(obj, key, value):
     """set attribute based on key value"""
     if key in ['created_at', 'updated_at']:
-        setattr(self, key, datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f'))
+        setattr(obj, key, datetime.datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f'))
     else:
-        setattr(self, key, value)
+        setattr(obj, key, value)
 
 
 class BaseModel:
@@ -24,9 +24,8 @@ class BaseModel:
                     set_attribute(self, key, value)
         else:
             self.id = str(uuid.uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = datetime.now()
-            storage.new(self)
+            self.created_at = datetime.datetime.now()
+            self.updated_at = datetime.datetime.now()
 
     def __str__(self):
         """this method returns string representation of instance object"""
@@ -38,6 +37,7 @@ class BaseModel:
 
         self.updated_at = datetime.now()
         storage.save()
+        storage.new(self)
 
     def to_dict(self):
         """ Method that converts instance attributes into a dictionary"""
